6c37e762e704de3985de94f9bd67c0cd0c3ad759
Jabadjuni <jakob.bisander@hotmail.com>
2017-05-05 10:36:06 +0200

    added GUI stuff

--- src/electroshop/FXMLDocumentController.java
+++ src/electroshop/FXMLDocumentController.java
@@ -8,14 +8,23 @@ package electroshop;
 import electroshop.persons.Visitor;
 import electroshop.persons.LoggedInPerson;
 import electroshop.connectors.AccountsConnector;
+import electroshop.connectors.ProductConnector;
 import electroshop.persons.Person;
 import java.net.URL;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.beans.property.ListProperty;
+import javafx.beans.property.SimpleListProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.Pane;
@@ -25,10 +34,11 @@ import javafx.scene.layout.Pane;
  * @author Jakob
  */
 public class FXMLDocumentController implements Initializable {
-    
+
     private Person activeUser;
     private LoggedInPerson loggedInPerson;
     private AccountsConnector accCon;
+    private ProductConnector prodCon;
 
     @FXML
     private Pane loginPane;
@@ -56,15 +66,69 @@ public class FXMLDocumentController implements Initializable {
     private Button createButton;
     @FXML
     private Label userInfoLabel;
+    @FXML
+    private ComboBox<String> categoryDrop;
+    @FXML
+    private Pane genPane;
+    @FXML
+    private Pane desktopPane;
+    @FXML
+    private ComboBox<String> integratedWifiCombo;
+    @FXML
+    private Pane computerPane;
+    @FXML
+    private Pane laptopPane;
+    @FXML
+    private Pane washingPane;
+    @FXML
+    private Pane fridgePane;
+    @FXML
+    private Pane tvRadioPane;
+    @FXML
+    private Pane tvPane;
+    @FXML
+    private Pane whiteGoodsPane;
+    @FXML
+    private Pane radioPane;
+    @FXML
+    private Pane generalPane;
+    @FXML
+    private Label column1Label;
+    @FXML
+    private Label column2Label;
+    @FXML
+    private Label column3Label;
+    @FXML
+    private Label column4Label;
+    @FXML
+    private Label column5Label;
+    @FXML
+    private Label column6Label;
+    @FXML
+    private TextField nameField;
+    @FXML
+    private TextField priceField;
+    @FXML
+    private TextField column1Field;
+    @FXML
+    private TextField column2Field;
+    @FXML
+    private TextField column3Field;
+    @FXML
+    private TextField column6Field;
+    @FXML
+    private TextField column5Field;
+    @FXML
+    private TextField column4Field;
 
     @FXML
     private void handleLogin(ActionEvent event) throws SQLException {
         String email = emailField.getText();
         String pw = pwField.getText();
-        
+
         loggedInPerson = accCon.login(email, pw, activeUser.getBasket());
-        
-        if(loggedInPerson == null){
+
+        if (loggedInPerson == null) {
             System.out.println("LOGIN NOT SUCCESFULL");
             loginPane.setStyle("-fx-background-color: #800000");
         } else {
@@ -72,27 +136,64 @@ public class FXMLDocumentController implements Initializable {
             //activeUser = accCon.login(email, pw, activeUser.getBasket());
             activeUser = loggedInPerson;
             loginPane.setVisible(false);
-            
+
             StringBuilder sb = new StringBuilder();
-            
-            
+
             sb.append(loggedInPerson.getEmail()).append(" ");
             sb.append(loggedInPerson.getTitle());
             userInfoLabel.setText(sb.toString());
             loggedInPane.setVisible(true);
         }
-        
-       
+
     }
-    
+
+    @FXML
+    private void changeSelection(ActionEvent event) throws SQLException {
+        String s = (String) categoryDrop.getValue();
+        ArrayList<String> list = prodCon.getColumns(s);
+        
+        int columnSize = list.size();
+        
+        genPane.setVisible(true);
+        computerPane.setVisible(false); laptopPane.setVisible(false); desktopPane.setVisible(false); whiteGoodsPane.setVisible(false); washingPane.setVisible(false); fridgePane.setVisible(false); tvRadioPane.setVisible(false); tvPane.setVisible(false); radioPane.setVisible(false);
+        
+        switch (s) {
+            case "laptops":                
+                computerPane.setVisible(true);
+                laptopPane.setVisible(true);
+                break;
+            case "desktops":
+                computerPane.setVisible(true);
+                desktopPane.setVisible(true);
+                break;
+            case "washingmachine":
+                whiteGoodsPane.setVisible(true);
+                washingPane.setVisible(true);
+                break;
+            case "fridge":
+                whiteGoodsPane.setVisible(true);
+                fridgePane.setVisible(true);
+                break;
+            case "tv":
+                tvRadioPane.setVisible(true);
+                tvPane.setVisible(true);
+                break;
+            case "radio":
+                tvRadioPane.setVisible(true);
+                radioPane.setVisible(true);
+                break;
+        }
+
+    }
+
     @FXML
     private void handleLogout(ActionEvent event) {
         loggedInPane.setVisible(false);
         loginPane.setVisible(true);
-        
+
         activeUser = new Visitor();
     }
-    
+
     @FXML
     private void createAccount(ActionEvent event) throws SQLException {
         String email = emailEntry.getText();
@@ -100,22 +201,32 @@ public class FXMLDocumentController implements Initializable {
         String address = addressEntry.getText();
         String phone = phoneEntry.getText();
         String pw = pwEntry.getText();
-        
-        if(Checker.checkAccount(email, name, phone) == true){
+
+        if (Checker.checkAccount(email, name, phone) == true) {
             accCon.insertAccount(email, name, phone, address, pw);
         } else {
             // Grafisk visning af fejl i account creation
             System.out.println("something is wrong");
         }
-        
+
     }
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        loggedInPane.setVisible(false);
         activeUser = new Visitor();
         //accCon = new AccountsConnector("jdbc:postgresql://localhost:5432/electroshop", "postgres", "2111993");
         accCon = new AccountsConnector("jdbc:postgresql://151.80.57.19:5432/admin_semester", "admin_willf", "2111993");
+        prodCon = new ProductConnector("jdbc:postgresql://151.80.57.19:5432/admin_semester", "admin_willf", "2111993");
+
+        try {
+            categoryDrop.setItems(prodCon.getCategories());
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+
+        ObservableList<String> options = FXCollections.observableArrayList("Yes","No");
+        integratedWifiCombo.setItems(options);
+
     }
 
 }
--- src/electroshop/connectors/ProductConnector.java
+++ src/electroshop/connectors/ProductConnector.java
@@ -5,15 +5,61 @@
  */
 package electroshop.connectors;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import javafx.beans.property.ListProperty;
+import javafx.beans.property.SimpleListProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
 /**
  *
  * @author Jakob
  */
-public class ProductConnector extends SuperDB{
+public class ProductConnector extends SuperDB {
 
     public ProductConnector(String host, String user, String pword) {
         super(host, user, pword);
     }
-    
-    
+
+    public ObservableList getCategories() throws SQLException {
+
+        ListProperty<String> listProperty = new SimpleListProperty<String>(FXCollections.<String>observableArrayList());
+
+        String cat = "SELECT table_name\n"
+                + "  FROM information_schema.tables\n"
+                + " WHERE table_schema='public'\n"
+                + "   AND table_type='BASE TABLE'\n"
+                + "   AND NOT table_name='accounts'\n"
+                + "   AND NOT table_name='products';";
+        PreparedStatement catSearch = this.getCon().prepareStatement(cat);
+        ResultSet rs = catSearch.executeQuery();
+
+        while (rs.next()) {
+            String tableName = rs.getString("table_name");
+            listProperty.add(tableName);
+        }
+        return listProperty;
+    }
+
+    public ArrayList getColumns(String tableName) throws SQLException {
+        ArrayList<String> list = new ArrayList<>();
+        
+        String col = "SELECT column_name\n"
+                + "FROM information_schema.columns\n"
+                + "WHERE table_name   = ?";
+        PreparedStatement colSearch = this.getCon().prepareStatement(col);
+        colSearch.setString(1, tableName);
+        
+        ResultSet rs = colSearch.executeQuery();
+        
+        while(rs.next()){
+            list.add(rs.getString("column_name"));
+            System.out.println(rs.getString("column_name"));
+        }
+        return list;
+    }
+
 }
